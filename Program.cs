717fa4710dc6ad3e44495e0a8ed7aad91e543099
imPeRia1 <97918570+imPeRia1@users.noreply.github.com>
2022-08-29 20:58:38 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int hourHandAmount = 0;
+int[] vasyaArray = { 1, -2 };
+AhShitHereWeGoAgain(vasyaArray, hourHandAmount);
+
+int[] AhShitHereWeGoAgain(int[] arr, int hHA)
+{
+    int[] results = new int[arr.Length + 1];
+    int minhHA = hHA;
+    for (int i = 0; i<results.Length; i++)
+    {
+        minhHA = hHA;
+        results[i] = hHA;
+        for (int j = i; j < arr.Length; j++)
+            results[j+1] = results[j] + arr[j];
+        
+        for (int x = i; x>0; x--)
+            results[x-1] = results[x] - arr[x-1];
+
+        for (int y = 0; y<results.Length; y++)
+            if (results[y] < hHA) minhHA = results[y];
+        if (minhHA == hHA) break;
+    }
+
+    if (minhHA != hHA) Console.WriteLine("not found");
+    else
+    {
+        foreach (var x in results)
+            Console.Write(x + " ");
+    }
+    return results;
+}
